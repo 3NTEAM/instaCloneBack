91666e5d81f0987cb9e6ad6e5d3b8cacf6e7f741
yeonhaaa <qkdxo101@naver.com>
2024-03-17 10:38:39 +0900

    add: yml 파일 db설정 추가

--- src/main/java/com/n/instaClone/InstaCloneApplication.java
+++ src/main/java/com/n/instaClone/InstaCloneApplication.java
@@ -2,7 +2,10 @@ package com.n.instaClone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+// db 없이 실행
+//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class InstaCloneApplication {
 
